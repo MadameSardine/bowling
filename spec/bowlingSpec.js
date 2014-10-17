22cf4f45f07e9feb712f2488a41044f2e162f75e
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-17 14:57:14 +0100

    Frame can have a second roll

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -13,18 +13,30 @@ describe('A Bowling game',function(){
 describe('A Bowling game frame',function(){
 
 	var frame;
+	beforeEach(function(){
+		frame = new Frame;
+
+	});
+
 
 	it('has rolls',function(){
-		frame = new Frame;
 		expect(frame.rolls).toEqual([]);
 
 	});
 
 	it('can have a first roll',function(){
-		frame = new Frame;
 		roll1 = new Roll;
 		frame.add_roll(roll1);
 		expect(frame.rolls).toEqual([roll1]);
+	});
+
+	it('can have a second roll',function(){
+		roll1 = new Roll;
+		roll2 = new Roll;
+		frame.add_roll(roll1);
+		frame.add_roll(roll2);
+		expect(frame.rolls).toEqual([roll1, roll2]);
+
 	});
 
 });
